287e074d0382b71b50a368a13f6fc08bb3c2e089
hjghgf <80122777+hjghgf@users.noreply.github.com>
2021-07-03 22:23:05 +0300

    Update Program.cs

--- 27ege/27ege/Program.cs
+++ 27ege/27ege/Program.cs
@@ -287,11 +287,6 @@ namespace _27ege
                 }
             }
 
-            int minDiff1_1 = 10001;
-            int minDiff1_2 = 10001;
-            int minDiff2_1 = 10001;
-            int minDiff2_2 = 10001;
-
             for (int i = 0; i < amount; i++)
             {
                 string[] inp = file.ReadLine().Split(" ");
@@ -341,7 +336,7 @@ namespace _27ege
             {
                 for (int j = i + 1; j < mas.Length; j++)
                 {
-                    if (mas[i] < mas[j])
+                    if (mas[i] > mas[j])
                     {
                         int temp = mas[i];
                         mas[i] = mas[j];
@@ -539,9 +534,106 @@ namespace _27ege
 
             file.Close();
         }
+        static void Task12()
+        {
+            var file = new StreamReader("..\\..\\..\\27.txt");
+            int lenght = int.Parse(file.ReadLine());
+            int p = 5;
+            int[,] max = new int[2, 2];
+            for (int k = 0; k < 2; k++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    max[k, j] = 0;
+                }
+            }
+
+            for (int i = 0; i < lenght; i++)
+            {
+                int a = int.Parse(file.ReadLine());
+                int r = a % 7 == 0 ? 1 : 0;
+                if (a > max[0, r])
+                {
+                    if (a % p == max[0, r] % p)
+                    {
+                        max[0, r] = a;
+                    }
+                    else
+                    {
+                        max[1, r] = max[0, r];
+                        max[0, r] = a;
+                    }
+                }
+                else if (a > max[1, r] && a <= max[0, r] && a % p != max[0, r] % p)
+                {
+                    max[1, r] = a;
+                }
+            }
+
+            int[] first = { max[0, 1], max[0, 1], max[0, 1], max[1, 1], max[1, 1] };
+            int[] second = { max[1, 1], max[0, 0], max[1, 0], max[0, 0], max[1, 0] };
+
+            int n1 = 0;
+            int n2 = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (first[i] != 0 && second[i] != 0 && first[i] + second[i] > n1 + n2)
+                {
+                    n1 = first[i];
+                    n2 = second[i];
+                }
+            }
+            Console.WriteLine(n1 + " " + n2);
+
+            file.Close();
+        }
+        static void Task33199()
+        {
+            var file = new StreamReader("..\\..\\..\\27.txt");
+
+            int length = int.Parse(file.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+            int sum3 = 0;
+            int minDiff = 10001;
+
+            for (int i = 0; i < length; i++)
+            {
+                string[] inp = file.ReadLine().Split();
+                int[] input = { int.Parse(inp[0]), int.Parse(inp[1]), int.Parse(inp[2]) };
+                Sorting(ref input);
+
+                sum1 += input[0];
+                sum2 += input[1];
+                sum3 += input[2];
+
+                if (input[2] - input[1] < minDiff && (input[2] - input[1]) % 2 != 0)
+                {
+                    minDiff = input[2] - input[1];
+                }
+                else if (input[2] - input[0] < minDiff && (input[2] - input[0]) % 2 != 0)
+                {
+                    minDiff = input[2] - input[0];
+                }
+            }
+            if (sum1 % 2 != sum2 % 2)
+            {
+                Console.WriteLine(sum3);
+            }
+            else if (minDiff != 10001)
+            {
+                Console.Write(sum3 - minDiff);
+            }
+            else
+            {
+                Console.WriteLine("haha");
+            }
+            file.Close();
+        }
         static void Main(string[] args)
         {
-            Task2671();
+            Task33199();
         }
     }
 }
